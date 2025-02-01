5ac4e7cce40ace7d4560ec29e0e6381d4eabb3d9
songwutt195 <songwut.ahm@gmail.com>
2025-02-01 15:35:57 +0000

    draft input

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 import streamlit as st
+from datetime import datetime, timedelta
+from lunarcalendar import Converter, Solar, Lunar
 import pandas as pd
+
+utc_now = datetime.utcnow()
+time_offset = timedelta(hours=7)
+thai_time = utc_now + time_offset
 
 st.title('BaZi Teller')
 st.header('BAZI Birth Chart')
 
-inf1, inf2, inf3 = st.columns(3)
+inf1, inf2, inf3 = st.columns(3, vertical_alignment="center")
 
 inf1.write('Your Birthday:')
 with inf2:
-  bd_val = st.date_input("", value=None)
-with in32:
-  bt_val = st.time_input("", value=None)
+  bd_val = st.date_input("", value=thai_time.date())
+with inf3:
+  bt_val = st.time_input("", value=thai_time.time())
 
 if st.button("Calculate"):
-    st.write(type(bd_val), type(bt_val))
+    st.write(thai_time.date(), thai_time.time())
+    st.write(thai_time)
